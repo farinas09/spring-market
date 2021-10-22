aca297cf42a355640ca1e51e95000e17d039b718
Erick Fariñas <efarinas99@gmail.com>
2021-10-21 21:21:59 -0600

    create repositories and setup application.properties

---
+++ src/main/java/com/farinas/market/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.farinas.market.repository;
+
+import com.farinas.market.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
+
+
+}
--- src/main/java/com/farinas/market/repository/ProductRepository.java
+++ src/main/java/com/farinas/market/repository/ProductRepository.java
@@ -1,2 +1,16 @@
-package com.farinas.market.repository;public interface ProductRepository {
+package com.farinas.market.repository;
+
+import com.farinas.market.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
+
+    //Convención sobre convicción
+    //CrudRepository permite realizar busquedas por campo según la entidad
+    Optional<ProductEntity> findByCategoryIdOrderByNameAsc(int categoryId);
+
 }
